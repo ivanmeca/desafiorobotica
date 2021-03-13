6164f573989fe5557a2d18e9a55c6903bbbda3e4
Ivan Mascarenhas <ivanmeca@gmail.com>
2021-03-12 22:39:58 -0300

    Controller creation

--- controllers/desafioRobotica/desafioRobotica.c
+++ controllers/desafioRobotica/desafioRobotica.c
@@ -1,24 +1,3 @@
-/*
- * Copyright 1996-2020 Cyberbotics Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Description:  A controller moving the Pioneer 3-DX and avoiding obstacles.
-                 3 LEDs are switched on and off periodically
- */
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,29 +7,28 @@
 #include <webots/motor.h>
 #include <webots/robot.h>
 
-// maximal speed allowed
-#define MAX_SPEED 5.24
+#define MAX_SPEED 5.24 // maximal speed allowed
+#define MAX_SENSOR_NUMBER 8 // how many sensors are on the robot
+#define DELAY 70 // delay used for the blinking leds
+#define MAX_SENSOR_VALUE 1024 // maximal value returned by the sensors
+#define MIN_DISTANCE 1.0 // minimal distance, in meters, for an obstacle to be considered
+#define WHEEL_WEIGHT_THRESHOLD 100 // minimal weight for the robot to turn
 
-// how many sensors are on the robot
-#define MAX_SENSOR_NUMBER 16
-
-// delay used for the blinking leds
-#define DELAY 70
-
-// maximal value returned by the sensors
-#define MAX_SENSOR_VALUE 1024
-
-// minimal distance, in meters, for an obstacle to be considered
-#define MIN_DISTANCE 1.0
-
-// minimal weight for the robot to turn
-#define WHEEL_WEIGHT_THRESHOLD 100
+#define LEFT_WHEEL 0
+#define RIGHT_WHEEL 1
 
 // structure to store the data associated to one sensor
 typedef struct {
     WbDeviceTag device_tag;
     double wheel_weight[2];
 } SensorData;
+// structure to store the data associated to the robot
+typedef struct {
+    double LeftWheelSpeed;
+    double LeftWheelWeight;
+    double RightWheelSpeed;
+    double RightWheelWeight;
+}Robot;
 
 // enum to represent the state of the robot
 typedef enum { FORWARD, LEFT, RIGHT } State;
@@ -58,9 +36,10 @@ typedef enum { FORWARD, LEFT, RIGHT } State;
 // how much each sensor affects the direction of the robot
 static SensorData sensors[MAX_SENSOR_NUMBER] = {
         {.wheel_weight = {150, 0}}, {.wheel_weight = {200, 0}}, {.wheel_weight = {300, 0}}, {.wheel_weight = {600, 0}},
-        {.wheel_weight = {0, 600}}, {.wheel_weight = {0, 300}}, {.wheel_weight = {0, 200}}, {.wheel_weight = {0, 150}},
-        {.wheel_weight = {0, 0}},   {.wheel_weight = {0, 0}},   {.wheel_weight = {0, 0}},   {.wheel_weight = {0, 0}},
-        {.wheel_weight = {0, 0}},   {.wheel_weight = {0, 0}},   {.wheel_weight = {0, 0}},   {.wheel_weight = {0, 0}}};
+        {.wheel_weight = {0, 600}}, {.wheel_weight = {0, 300}}, {.wheel_weight = {0, 200}}, {.wheel_weight = {0, 150}}};
+
+State DefineDirection(State ActualDirection,Robot *robot);
+void TreatSensors(Robot *robot);
 
 int main() {
     // necessary to initialize Webots
@@ -72,12 +51,6 @@ int main() {
     // stores device IDs for the wheels
     WbDeviceTag left_wheel = wb_robot_get_device("left wheel");
     WbDeviceTag right_wheel = wb_robot_get_device("right wheel");
-
-    // stores device IDs for the LEDs
-    WbDeviceTag red_led[3];
-    red_led[0] = wb_robot_get_device("red led 1");
-    red_led[1] = wb_robot_get_device("red led 2");
-    red_led[2] = wb_robot_get_device("red led 3");
 
     char sensor_name[5] = "";
     int i;
@@ -95,99 +68,83 @@ int main() {
     wb_motor_set_velocity(left_wheel, 0.0);
     wb_motor_set_velocity(right_wheel, 0.0);
 
-    int j, led_number = 0, delay = 0;
-    double speed[2] = {0.0, 0.0};
-    double wheel_weight_total[2] = {0.0, 0.0};
-    double distance, speed_modifier, sensor_value;
-
     // by default, the robot goes forward
     State state = FORWARD;
+    Robot robot1;
 
     // run simulation
     while (wb_robot_step(time_step) != -1) {
-        // initialize speed and wheel_weight_total arrays at the beginning of the loop
-        memset(speed, 0, sizeof(double) * 2);
-        memset(wheel_weight_total, 0, sizeof(double) * 2);
-
-        for (i = 0; i < MAX_SENSOR_NUMBER; ++i) {
-            sensor_value = wb_distance_sensor_get_value(sensors[i].device_tag);
-
-            // if the sensor doesn't see anything, we don't use it for this round
-            if (sensor_value == 0.0)
-                speed_modifier = 0.0;
-            else {
-                // computes the actual distance to the obstacle, given the value returned by the sensor
-                distance = 5.0 * (1.0 - (sensor_value / MAX_SENSOR_VALUE));  // lookup table inverse.
-
-                // if the obstacle is close enough, we may want to turn
-                // here we compute how much this sensor will influence the direction of the robot
-                if (distance < MIN_DISTANCE)
-                    speed_modifier = 1 - (distance / MIN_DISTANCE);
-                else
-                    speed_modifier = 0.0;
-            }
-
-            // add the modifier for both wheels
-            for (j = 0; j < 2; ++j)
-                wheel_weight_total[j] += sensors[i].wheel_weight[j] * speed_modifier;
-        }
-
-        // (very) simplistic state machine to handle the direction of the robot
-        switch (state) {
-            // when the robot is going forward, it will start turning in either direction when an obstacle is close enough
-            case FORWARD:
-                if (wheel_weight_total[0] > WHEEL_WEIGHT_THRESHOLD) {
-                    speed[0] = 0.7 * MAX_SPEED;
-                    speed[1] = -0.7 * MAX_SPEED;
-                    state = LEFT;
-                } else if (wheel_weight_total[1] > WHEEL_WEIGHT_THRESHOLD) {
-                    speed[0] = -0.7 * MAX_SPEED;
-                    speed[1] = 0.7 * MAX_SPEED;
-                    state = RIGHT;
-                } else {
-                    speed[0] = MAX_SPEED;
-                    speed[1] = MAX_SPEED;
-                }
-                break;
-                // when the robot has started turning, it will go on in the same direction until no more obstacle are in sight
-                // this will prevent the robot from being caught in a loop going left, then right, then left, and so on.
-            case LEFT:
-                if (wheel_weight_total[0] > WHEEL_WEIGHT_THRESHOLD || wheel_weight_total[1] > WHEEL_WEIGHT_THRESHOLD) {
-                    speed[0] = 0.7 * MAX_SPEED;
-                    speed[1] = -0.7 * MAX_SPEED;
-                } else {
-                    speed[0] = MAX_SPEED;
-                    speed[1] = MAX_SPEED;
-                    state = FORWARD;
-                }
-                break;
-            case RIGHT:
-                if (wheel_weight_total[0] > WHEEL_WEIGHT_THRESHOLD || wheel_weight_total[1] > WHEEL_WEIGHT_THRESHOLD) {
-                    speed[0] = -0.7 * MAX_SPEED;
-                    speed[1] = 0.7 * MAX_SPEED;
-                } else {
-                    speed[0] = MAX_SPEED;
-                    speed[1] = MAX_SPEED;
-                    state = FORWARD;
-                }
-                break;
-        }
-
-        // the three red LEDs are swicthed on and off periodically
-        ++delay;
-        if (delay == DELAY) {
-            wb_led_set(red_led[led_number], 0);
-            ++led_number;
-            led_number = led_number % 3;
-            wb_led_set(red_led[led_number], 1);
-            delay = 0;
-        }
+        TreatSensors(&robot1);
+        state = DefineDirection(state,&robot1);
         // sets the motor speeds
-        wb_motor_set_velocity(left_wheel, speed[0]);
-        wb_motor_set_velocity(right_wheel, speed[1]);
+        wb_motor_set_velocity(left_wheel, robot1.LeftWheelSpeed);
+        wb_motor_set_velocity(right_wheel, robot1.RightWheelSpeed);
     }
 
     wb_robot_cleanup();
 
     return 0;
+}
+
+State DefineDirection(State ActualDirection,Robot *robot){
+    switch (ActualDirection) {
+        case FORWARD:
+            if (robot->LeftWheelWeight > WHEEL_WEIGHT_THRESHOLD) {
+                robot->LeftWheelSpeed = 0.7 * MAX_SPEED;
+                robot->RightWheelSpeed = -0.7 * MAX_SPEED;
+                ActualDirection = LEFT;
+            } else if (robot->RightWheelWeight > WHEEL_WEIGHT_THRESHOLD) {
+                robot->LeftWheelSpeed = -0.7 * MAX_SPEED;
+                robot->RightWheelSpeed = 0.7 * MAX_SPEED;
+                ActualDirection = RIGHT;
+            } else {
+                robot->LeftWheelSpeed = MAX_SPEED;
+                robot->RightWheelSpeed = MAX_SPEED;
+            }
+            break;
+        case LEFT:
+            if (robot->LeftWheelWeight > WHEEL_WEIGHT_THRESHOLD || robot->RightWheelWeight > WHEEL_WEIGHT_THRESHOLD) {
+                robot->LeftWheelSpeed = 0.7 * MAX_SPEED;
+                robot->RightWheelSpeed = -0.7 * MAX_SPEED;
+            } else {
+                robot->LeftWheelSpeed = MAX_SPEED;
+                robot->RightWheelSpeed = MAX_SPEED;
+                ActualDirection = FORWARD;
+            }
+            break;
+        case RIGHT:
+            if (robot->LeftWheelWeight > WHEEL_WEIGHT_THRESHOLD || robot->RightWheelWeight > WHEEL_WEIGHT_THRESHOLD) {
+                robot->LeftWheelSpeed = -0.7 * MAX_SPEED;
+                robot->RightWheelSpeed = 0.7 * MAX_SPEED;
+            } else {
+                robot->LeftWheelSpeed = MAX_SPEED;
+                robot->RightWheelSpeed = MAX_SPEED;
+                ActualDirection = FORWARD;
+            }
+            break;
+    }
+    return ActualDirection;
+}
+
+void TreatSensors(Robot *robot) {
+    int i;
+    double distance,speed_modifier, sensor_value;
+
+    robot->LeftWheelWeight =0;
+    robot->RightWheelWeight=0;
+
+    for (i = 0; i < MAX_SENSOR_NUMBER; ++i) {
+        sensor_value = wb_distance_sensor_get_value(sensors[i].device_tag);
+        if (sensor_value == 0.0)
+            speed_modifier = 0.0;
+        else {
+            distance = 5.0 * (1.0 - (sensor_value / MAX_SENSOR_VALUE));  // lookup table inverse.
+            if (distance < MIN_DISTANCE)
+                speed_modifier = 1 - (distance / MIN_DISTANCE);
+            else
+                speed_modifier = 0.0;
+        }
+        robot->LeftWheelWeight += sensors[i].wheel_weight[LEFT_WHEEL] * speed_modifier;
+        robot->RightWheelWeight += sensors[i].wheel_weight[RIGHT_WHEEL] * speed_modifier;
+    }
 }
